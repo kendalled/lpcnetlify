9f99809741aebefd4d0b0f3caf028ac606c070ae
Kendall Jackson <kendallkj00@gmail.com>
2021-05-01 17:14:12 -0400

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div class="pt-10 px-4 flex flex-col min-h-screen items-center justify-center">
+      <img class="h-8 mb-4 w-auto mx-auto" src="~/static/lapel.svg">
+      <h1 class="text-xl text-center font-medium text-gray-700">
+        We're working to improve our site.
+      </h1>
+
+      <h2 class="text-center max-w-md mt-1 text-gray-600">
+        Pardon our dust. It will be back and better than ever in no time. You can reach us below.
+      </h2>
+
+      <div class="mt-8 flex sm:flex-row">
+        <div class="inline-flex rounded-md shadow">
+          <a href="mailto:info@lapelpinsandcoins.com" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">
+            Send an email
+          </a>
+        </div>
+        <div class="ml-3 inline-flex">
+          <a href="tel:+18443954464" target="_blank" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200">
+            Call support
+          </a>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
